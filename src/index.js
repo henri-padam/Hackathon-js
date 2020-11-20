e2de955cd3db9684c209000c5973cbf40b0fdbfe
Henri <henri@padam.io>
2020-11-20 09:23:06 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+console.log('======================== read text ======')
+fs.readFile('./data/dataset1.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log({ err })
+    return
+  }
+  console.log({ data })
+})
+
+console.log('=================== parse a json =====')
+fs.readFile('./data/json1.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log({ err })
+    return
+  }
+  const json = JSON.parse(data)
+  console.log({ json })
+  console.log(typeof json)
+  console.log({ 'json.array': json.array })
+})
