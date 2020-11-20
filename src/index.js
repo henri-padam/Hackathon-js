148057b46bfb3f8a962cae217048c299fe9ba730
Henri <henri@padam.io>
2020-11-20 10:59:40 +0100

    pipe ok

--- src/index.js
+++ src/index.js
@@ -1,32 +1,55 @@
 import fs from 'fs'
 import _ from 'lodash'
-console.log('======================== read text ======')
-fs.readFile('./data/dataset1.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.log({ err })
-    return
-  }
-  console.log({ data })
-})
 
-console.log('=================== parse a json =====')
-fs.readFile('./data/json1.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.log({ err })
-    return
-  }
+fs.readFile('./data/instances.json', 'utf8', (err, data) => {
   const json = JSON.parse(data)
-  console.log({ json })
-  console.log(typeof json)
-  console.log({ 'json.array': _.get(json, 'array') })
-})
+  const soluce = {}
+  for (let i = 1; i <= 10; i++) {
+    const maison = json[i]
+    // maison.items
+    // maison.capacity
+    if (i === 1) console.log({ maison })
+    // rentabilité massique : prix par kilo
+    const rM = maison.values.map((valeurObjet, i) => {
+      return valeurObjet / maison.weights[i]
+    })
+    if (i === 1) console.log({ rM })
+    // indexes to sort
+    const indexes = []
+    for (let j = 0; j < maison.items; j++) {
+      indexes.push(j)
+    }
+    if (i === 1) console.log({ indexes })
+    const indexes2 = [...indexes]
+    // sort
+    indexes2.sort((a, b) => {
+      return (rM[b] - rM[a])
+    })
+    if (i === 1) console.log({ indexes2 })
 
-console.log('================ write text ==========')
-fs.writeFile(
-  './output/helloworld.txt',
-  'Hello World!',
-  function (err) {
-    if (err) return console.log(err)
-    console.log('Hello World > helloworld.txt')
+    // indexes of more expensive per kilo first
+    const solutionMaison = new Array(maison.items)
+    let poidRestant = maison.capacity
+    const maisonSolution = indexes2.map(k => {
+      const poid = maison.weights[k]
+      if (i === 1) console.log({ poid })
+      if (poid <= poidRestant) {
+        poidRestant -= poidRestant
+        solutionMaison[k] = true
+      } else {
+        solutionMaison[k] = false
+      }
+    })
+    if (i === 1) console.log({ solutionMaison })
+    for (let l = 0; l < maison.items; l++) {
+      if (solutionMaison[l] && i === 1) {
+        console.log(l)
+      }
+    }
+    // solutionMaison.find
+
+    soluce[i] = solutionMaison
   }
-)
+  console.log({ soluce })
+  fs.writeFile('./output/solution.json', JSON.stringify(soluce), err => console.log(err))
+})
